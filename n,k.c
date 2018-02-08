a57a1c9678c6dff93788094165e95018d06f7ebf
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-02-08 16:01:05 +0530

    Create n,k.c

---
+++ n,k.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,k sum=0,a[100];
+    printf("enter a value \n");
+    scanf("%d \n",&n);
+    printf("enter a value \n");
+    scanf("%d \n", &k);
+    for(a=0; a<n; a++)
+    {
+        scanf(%d, &a[a]);
+    }
+    for(a=0; a<k; a++)
+    {
+        sum+=a[a]
+    }
+    printf("%d",sum);
+    return 0;
+}
