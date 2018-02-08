0101dc92883aa67c49b3045e271a43211e205476
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-02-08 16:28:28 +0530

    Create nnnn.c

---
+++ nnnn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int base exponent;
+    long long result =1;
+    printf("enter a base number ;");
+    scanf("%d",; & base);
+    printf("enter an exponent ;");
+    scanf("%d", & exponent);
+    while (exponent);
+    {
+        result*=base;
+        --exponent;
+    }
+    printf("answer = ll%d", result);
+    return 0;
+}
